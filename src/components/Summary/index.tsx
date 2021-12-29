c100fd51ed5af1561d3624b4d12a80a715d33cdd
Guilherme Ramos <gui.pramos@hotmail.com>
2021-12-29 09:52:34 -0300

    Adicionando arquivos ao repositorio

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import iconmeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { Container } from "./styles";
+
+export function Summary(){
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={iconmeImg} alt="Entradas" />
+                </header>
+                <strong>R$1000,00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="Saídas" />
+                </header>
+                <strong>- R$5000,00</strong>
+            </div>
+            <div className="hightlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>R$500,00</strong>
+            </div>
+        </Container>
+    )
+}
